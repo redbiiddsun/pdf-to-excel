30eb158677c78dda8a9877059dfd7968f28ca7a3
Phanasorn Srisayam <sunphanasorn@gmail.com>
2023-06-09 16:22:44 +0700

    just test

---
+++ pykit.py
@@ -0,0 +1,3 @@
+import pdfkit 
+
+pdfkit.from_file('/mnt/c/Users/tgsav/OneDrive/Desktop/pdf-to-excel/test/test2.pdf', '/mnt/c/Users/tgsav/OneDrive/Desktop/pdf-to-excel/test/index.html')
--- test.py
+++ test.py
@@ -1,13 +1,19 @@
-from pythainlp.util import normalize
-from pythainlp import spell
-from pythainlp import correct
+# from pythainlp.util import normalize
+# from pythainlp import spell
+# from pythainlp import correct
 
 
-text = "จ ี"
+# text = "จ ี"
 
-text = correct(text)
+# text = correct(text)
 
 
+# print(text)
+
+
+import fitz
+doc = fitz.open('C:\Users\tgsav\OneDrive\Desktop\pdf-to-excel/test/test2.pdf')
+text = ""
+for page in doc:
+   text+=page.get_text()
 print(text)
-
-
