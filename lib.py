7e8e6b1b95f7e06957dfb9c9cd5abe228701e6bb
Phanasorn Srisayam <sunphanasorn@gmail.com>
2023-06-12 14:55:59 +0700

    uncomment code

--- lib.py
+++ lib.py
@@ -1,19 +1,19 @@
-# import tabula
-# import pandas as pd
+import tabula
+import pandas as pd
 
-# def convertAllInDirectory(dir, format):
-#     try:
-#         tabula.convert_into_by_batch(dir, output_format=format, pages='all')
-#     except Exception as e:
-#         print(e)
+def convertAllInDirectory(dir, format):
+    try:
+        tabula.convert_into_by_batch(dir, output_format=format, pages='all')
+    except Exception as e:
+        print(e)
 
-# def convertFileIntoSeparate(dir, format):
-#     try:
-#         tabula.convert_into_by_batch(dir, output_format=format, pages='all')
-#     except Exception as e:
-#         print(e)
+def convertFileIntoSeparate(dir, format):
+    try:
+        tabula.convert_into_by_batch(dir, output_format=format, pages='all')
+    except Exception as e:
+        print(e)
 
 
 
-# # convertAllInDirectory("/mnt/c/Users/Phanasorn/Desktop/PJ/test", "csv")    
-# tabula.convert_into("/mnt/c/Users/Phanasorn/Desktop/PJ/test/MOCK_DATA_SEVERALPAGE.pdf", "output.csv", output_format="csv", pages='all')
+convertAllInDirectory("/mnt/c/Users/tgsav/OneDrive/Desktop/pdf-to-excel/test/", "csv")    
+#tabula.convert_into("/mnt/c/Users/Phanasorn/Desktop/PJ/test/MOCK_DATA_SEVERALPAGE.pdf", "output.csv", output_format="csv", pages='all')
