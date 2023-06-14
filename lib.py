00bdea2785cbb59c6cc697a7237c87cb9a022b57
Redbiidd.sun <38393603+redbiiddsun@users.noreply.github.com>
2023-06-14 16:34:52 +0700

    add word correction (processing)

--- lib.py
+++ lib.py
@@ -1,5 +1,10 @@
+import sys
+import PyPDF2
 import tabula
 import pandas as pd
+import difflib
+import re
+
 
 def convertAllInDirectory(dir, format):
     try:
@@ -13,7 +18,64 @@ def convertFileIntoSeparate(dir, format):
     except Exception as e:
         print(e)
 
+def find_similar_word(word, word_list):
+    matches = difflib.get_close_matches(word, word_list)
+    return matches
+
+def correct_wordlist(dir):
+    try:
+        reader = PyPDF2.PdfReader(r"/mnt/c/Users/tgsav/OneDrive/Desktop/pdf-to-excel/test/test2.pdf")
+        number_of_pages = len(reader.pages)
+        page = reader.pages[0]
+        text = page.extract_text()
+
+        li = re.split(r'\s+|\n', text)
+    
+        return li
+
+    except Exception as e:
+        print(e)
+        sys.exit()
+
+dirs = r"/mnt/c/Users/tgsav/OneDrive/Desktop/pdf-to-excel/test/Pre-Freshy_2022.pdf"
+
+df = tabula.read_pdf(dirs, encoding = 'utf-8' , pages='all')
+wordlist = correct_wordlist(dirs)
+list = []
+
+for i in df:
+    list.append(i.values.tolist())
+
+def find_similar_word(word, word_list):
+    # print (word)
+    try:
+        matches = difflib.get_close_matches(word, word_list, n=1)
+        return word if len(matches) == 0 else matches[0]
+    except Exception as e:
+        return word
+    
+    
+
+for i in range(len(list)):
+    for x in range(len(list[i])):
+        for k in range(len(list[i][x])):
+            
+            if(str(list[i][x][k]).isnumeric() == True):
+                continue
+            else:
+                print("text")
+                list[i][x][k] = find_similar_word(list[i][x][k], wordlist)
+                print(find_similar_word(list[i][x][k], wordlist))
+            print (list[i][x][k])
 
 
-convertAllInDirectory("/mnt/c/Users/tgsav/OneDrive/Desktop/pdf-to-excel/test/", "csv")    
-#tabula.convert_into("/mnt/c/Users/Phanasorn/Desktop/PJ/test/MOCK_DATA_SEVERALPAGE.pdf", "output.csv", output_format="csv", pages='all')
+# # print(list)
+
+# my_list = [l for li in list for l in li]
+# print(my_list)
+# df = pd.DataFrame(my_list)
+
+# print(df)
+
+# convertAllInDirectory("/mnt/c/Users/tgsav/OneDrive/Desktop/pdf-to-excel/test/", "csv")    
+# tabula.convert_into("/mnt/c/Users/Phanasorn/Desktop/PJ/test/MOCK_DATA_SEVERALPAGE.pdf", "output.csv", output_format="csv", pages='all')
